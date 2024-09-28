c88289b68a9959b6ef4b94480098b357e1959212
Omar Azmi <omar.aazmi@gmail.com>
2024-09-28 08:03:38 +0300

    - add a simple test for the server.   run the test via the shell command: `rails test test/controllers/v1/blobs_controller_test.rb` (this is minitest, not rspec). - the server itself can be run via the shell command `rails server`.

--- test/controllers/v1/blobs_controller_test.rb
+++ test/controllers/v1/blobs_controller_test.rb
@@ -4,4 +4,40 @@ class V1::BlobsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  # Setup method to initialize any shared variables (e.g., test payloads)
+  def setup
+    @valid_id = "test_blob7"
+    @valid_payload = {
+      id: @valid_id,
+      data: Base64.strict_encode64("Hello, world!")
+    }.to_json
+    @invalid_payload = { foo: "bar" }.to_json
+  end
+
+  # Test valid POST request
+  test "should create blob with valid payload" do
+    post("/v1/blobs", params: @valid_payload, headers: { "Content-Type": "application/json", "Authorization": "Bearer my-secret-token"  })
+    assert_response :created
+    assert_includes @response.body, "Blob stored successfully"
+  end
+
+  # Test invalid POST request
+  test "should return unprocessable_entity with invalid payload" do
+    post("/v1/blobs", params: @invalid_payload, headers: { "Content-Type": "application/json" })
+    assert_response :unprocessable_entity
+    assert_includes @response.body, "Incorrect json body provided"
+  end
+
+  test "should get blob by id" do
+    get("/v1/blobs/#{@valid_id}", headers: { "Authorization": "Bearer my-secret-token" })
+    assert_response :ok
+    assert_match "Hello, world!", Base64.strict_decode64(JSON.parse(@response.body)["data"])
+  end
+
+  test "should return not_found for non-existing blob" do
+    get("/v1/blobs/#{"non_existing_blob"}", headers: { "Authorization": "Bearer some-token" })
+    assert_response :not_found
+    assert_includes @response.body, "Object not found"
+  end
 end
